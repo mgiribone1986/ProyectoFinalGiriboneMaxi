61aad93f8c0d4d2e009d9e6ff0a5fc1600a638c8
Maxi Giribone <giribonemaximiliano@gmail.com>
2024-05-25 09:48:34 -0300

    Entrega Final ver 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { DashboardModule } from './layouts/dashboard/dashboard.module';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 // import { DashboardComponent } from './layouts/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, DashboardModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, DashboardModule, HttpClientModule, StoreModule.forRoot({}, {})],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
